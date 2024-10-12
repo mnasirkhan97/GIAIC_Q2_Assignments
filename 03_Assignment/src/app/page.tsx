8fabc21dd966c1be27344eeaf3efbd63635a4a71
M Nasir Khan <immnk97@gmail.com>
2024-10-13 01:49:27 +0500

    Assignment 03 code

---
+++ 03_Assignment/src/app/page.tsx
@@ -0,0 +1,31 @@
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">Welcome to Our Website</h1>
+        <p className="mt-4 text-lg">
+          At our company, we prioritize customer satisfaction and aim to provide
+          top-notch services that cater to your needs.
+        </p>
+        {/* <p className="mt-4 text-lg">
+          Whether you are looking for innovative solutions or expert guidance,
+          our dedicated team is here to help.
+        </p> */}
+        {/* <p className="mt-4 text-lg">
+          Explore our services, learn more about us, or get in touch today to
+          see how we can assist you in achieving your goals.
+        </p>
+        <div className="mt-8">
+          <a
+            href="/services"
+            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+          >
+            Explore Our Services
+          </a>
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
