830d6bce0715cd466c95ef5a65d1dc58791d86b6
M Nasir Khan <immnk97@gmail.com>
2024-10-13 01:49:32 +0500

    Assignment 03 code

---
+++ 03_Assignment/src/app/about/page.tsx
@@ -0,0 +1,18 @@
+const About = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">About Us</h1>
+        <p className="mt-4 text-lg">
+          We are a company dedicated to providing quality services to our
+          clients. Our mission is to deliver exceptional solutions tailored to
+          your needs. With years of experience and a team of skilled
+          professionals, we strive to exceed expectations and build long-lasting
+          relationships.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
